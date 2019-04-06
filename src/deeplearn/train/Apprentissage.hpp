e7c32af4a643d6363a473ebcb32c7b67c2ada3d4
david <david.albert@insa-rouen.fr>
2019-04-06 11:05:43 +0200

    Initialisation package avec classe non implémentée

---
+++ src/deeplearn/train/Apprentissage.hpp
@@ -0,0 +1,22 @@
+#ifndef __APPRENTISSAGE_HPP__
+#define __APPRENTISSAGE_HPP__
+
+
+/** 
+ * \class Apprentissage 
+ * \brief Gère l'apprentissage automatique des paramètres du réseau de neurones.
+ * \author ???
+ * \version 1.0 
+ * \date avril 2019
+ *
+ * Module permettant le lancement de l'apprentissage.
+ * Il permet également la récupération des données et paramètres d'apprentissage.
+ * 
+ * 
+ */
+class Apprentissage {
+
+    
+};
+
+#endif
---
+++ src/pretraitement/Pretraitement.hpp
@@ -0,0 +1,21 @@
+#ifndef __PRETRAITEMENT_HPP__
+#define __PRETRAITEMENT_HPP__
+
+
+/** 
+ * \class Pretraitement
+ * \brief ???
+ * \author ???
+ * \version 1.0 
+ * \date avril 2019
+ *
+ * ???????
+ * 
+ * 
+ */
+class Pretraitement {
+
+    
+};
+
+#endif
