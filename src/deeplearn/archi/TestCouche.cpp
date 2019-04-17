6bb74ceffe3c84f8272887c065c0aa0e98ed73ed
Maouchi Kevin <kevin.maouchi@insa-rouen.fr>
2019-04-17 11:39:13 +0200

    Modifs testCouche et ajout testTenseur

--- src/deeplearn/archi/TestCouche.cpp
+++ src/deeplearn/archi/TestCouche.cpp
@@ -12,11 +12,15 @@ public:
 	
 	d1 = new DimTenseur(2);
 	d2 = new DimTenseur(3);
+	d3 = new DimTenseur(4);
+	d4 = new DimTenseur(5);
 	c1 = new Couche (d1,d1);
 	c2 = new Couche (d1,d2);
 	t1 = new Tenseur(2);
 
 };
+
+
 	void tearDown()
 {
 
@@ -28,6 +32,59 @@ public:
 
 };
 
+
+	void testGetDimInput()
+{
+
+	CPPUNIT_ASSERT (c1.getDimInput()==d1);
+	CPPUNIT_ASSERT (!c1.getDimInput()==d2);
+	CPPUNIT_ASSERT (c2.getDimInput()==d1);
+	CPPUNIT_ASSERT (!c1.getDimInput()==d2);
+
+};
+
+
+	void testGetDimOutput()
+{
+
+	CPPUNIT_ASSERT (c1.getDimOutput()==d1);
+	CPPUNIT_ASSERT (!c1.getDimOutput()==d2);
+	CPPUNIT_ASSERT (c2.getDimOutput()==d2);
+	CPPUNIT_ASSERT (!c2.getDimOutpout()==d1);
+
+};
+
+
+	void testSetDimInput()
+{
+
+	c1.setDimInput(d3);
+	c2.setDimInput(d4);
+	
+
+	CPPUNIT_ASSERT (c1.getDimInput()==d3);
+	CPPUNIT_ASSERT (!c1.getDimInput()==d2);
+	CPPUNIT_ASSERT (c2.getDimInput()==d4);
+	CPPUNIT_ASSERT (!c2.getDimInput()==d1);
+
+};
+
+
+	void testSetDimOutput()
+{
+
+	c1.setDimOutput(d3);
+	c2.setDimOutput(d4);
+	
+
+	CPPUNIT_ASSERT (c1.getDimOutput()==d3);
+	CPPUNIT_ASSERT (!c1.getDimOutput()==d2);
+	CPPUNIT_ASSERT (c2.getDimOutput()==d4);
+	CPPUNIT_ASSERT (!c2.getDimOutput()==d1);
+
+};
+
+
 	void testPropagation()
 {
 
@@ -35,9 +92,12 @@ public:
 	CPPUNIT_ASSERT (c2.propagation(t1).getDim()==c2.getDimOutput());
 
 };
+
+
 	void testDerivee()
 {
-
+	CPPUNIT_ASSERT (c1.derivee(t1).getDim()==c1.getDimOutput());
+	CPPUNIT_ASSERT (c2.derivee(t1).getDim()==c2.getDimOutput());
 };
 
 
--- src/deeplearn/archi/TestCouche.hpp
+++ src/deeplearn/archi/TestCouche.hpp
@@ -4,7 +4,6 @@
 #include "cppunit/TestFixture.h"
 #include "Tenseur.hpp"
 #include "DimTenseur.hpp"
-#include "Graphe.hpp"
 
 
 using namespace CppUnit;
@@ -16,11 +15,15 @@ private :
 	Couche c1,c2;
 	Tenseur t1;
 	DimTenseur d1,d2
-	Graphe g1;
+
 public:
 	void setUp();
 	void tearDown();
 
+	void testGetDimInput();
+	void testGetDimOutput();
+	void testSetDimInput();
+	void testSetDimOutput();
 	void testPropagation();
 	void testDerivee();
 	
