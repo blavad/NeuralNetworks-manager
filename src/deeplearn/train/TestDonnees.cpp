1dbd60eb8de99a0ebbabd79884c2404447516986
syoussouf <samra.youssouf@insa-rouen.fr>
2019-04-23 12:42:35 +0200

    modification des methodes testAjouterDonnnees et testAjouterDonnee

--- src/deeplearn/train/TestDonnees.cpp
+++ src/deeplearn/train/TestDonnees.cpp
@@ -1,29 +1,22 @@
 #include "TestDonnees.hpp"
-
+#include <cppunit/TestFixture.h>
+#include <cppunit/TestAssert.h>
 using namespace CppUnit;
 
-class TestDonnees : public CppUnit::TestFixture{
+	Donnees d1;
+	std::vector<Donnee> d2;
+	Donnees d3;
+	Donnee d;
 
-    void setUp(){
-	d1 = new Donnees();
-	std::vector<Donnee> d2
-    };
-
-    void tearDown(){
-      	delete d1;
-      	delete d2;
-    };
-
-    void testAjouterDonnees(){
+    void TestDonnees::testAjouterDonnees(){
+	int n=d1.Donnees::getNbDonnees();
         d1.ajouterDonnees(d2);
-        CPPUNIT_ASSERT ((d1.getNbDonnees() == d2.getNbDonnees()) && (d1.getDonnees() == d2.getDonnees());
+	CPPUNIT_ASSERT(d1.Donnees::getNbDonnees()==d2.size()+n);
     };
 
-    void testMelanger(){
-	d1.ajouterDonnees(d2);
-	d1.melanger();
-        CPPUNIT_ASSERT (d1.getDonnees() != d2);
+    void TestDonnees::testAjouterDonnee(){
+        d1.ajouterDonnee(d);
+	int n=d1.Donnees::getNbDonnees();
+	CPPUNIT_ASSERT(d1.Donnees::getNbDonnees()==n+1);
     };
 
-
-};
