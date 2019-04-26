76a82ecf201ed1bd381d254ecd6f489b1f0dc728
david <david.albert@insa-rouen.fr>
2019-04-26 16:24:50 +0200

    Ajouter main page

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+/* Documentation tag for Doxygen
+ */
+
+/*! \mainpage PLEGAT Documentation
+ *
+ * \section intro_sec Introduction
+ *
+ * This is the Plegat documentation.
+ *
+ * \section install_sec Installation
+ *
+ * \subsection tools_subsec Tools required&#58;
+ * - Java Runtime Environment &#40;JRE&#41; 1.4.2 or later &#40;<small><a href="http&#58;//java.sun.com/">Java Web Site etc etc...
+ *
+ * \subsection running Running the program
+ * In a command window, etc etc...
+ *
+ * \section copyright Copyright and License
+ * This license applies to etc etc...
+ *
+ * <BR><BR>
+ *
+ */
+
+int main(){
+    return 0;
+}
