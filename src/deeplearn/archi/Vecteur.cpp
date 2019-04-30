97d3bc4378572e3dce83c0eb86d70764db3c183f
Marion <marion.schaeffer@insa-rouen.fr>
2019-04-30 13:48:28 +0200

    Modif vecteur herite de matrice

--- src/deeplearn/archi/Vecteur.cpp
+++ src/deeplearn/archi/Vecteur.cpp
@@ -1,12 +1,4 @@
 #include "Vecteur.hpp"
 
 
-Vecteur::Vecteur(int l):Tenseur(std::vector<int>{l})
-{
-    
-}
-
-Vecteur::Vecteur(double* vl, int l):Tenseur(DimTenseur(std::vector<int>{l}))
-{
-    setValeurs(vl);
-}
+Vecteur::Vecteur(int l):Matrice(l,1){}
