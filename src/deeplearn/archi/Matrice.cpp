338eadc4f1e65456004ef29fa2d2263edcab5dd1
Marion <marion.schaeffer@insa-rouen.fr>
2019-04-30 13:08:27 +0200

    ajout produit vectoriel avec redef *

--- src/deeplearn/archi/Matrice.cpp
+++ src/deeplearn/archi/Matrice.cpp
@@ -8,3 +8,19 @@ Matrice::Matrice(double *vl, int l, int c) : Tenseur(DimTenseur(std::vector<int>
 {
     setValeurs(vl);
 }
+
+Tenseur Matrice::operator*(const Tenseur &t)
+{
+    if (getDim().getDim(1) != t.getDim().getDim(0)) throw DimensionsIncompatiblesException();
+    else
+    {
+        for(int i=0; i++ ; i<getDim().getDim(0)){
+            for(int j=0; j++ ; j<t.getDim().getDim(1)){
+                for(int k=0; k++ ; k<getDim().getDim(1)){
+                    setValeur(getValeur(std::vector<int>{i,j})+ getValeur(std::vector<int>{i,k}) * t.getValeur(std::vector<int>{k,j}),std::vector<int>{i,j});
+                }
+            }
+        }
+    }
+    
+}
